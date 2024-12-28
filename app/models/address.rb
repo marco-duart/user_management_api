1459e864638c8a21255031555bff672b1a0489e4
marco-duart <marcoaurelio_db@hotmail.com>
2024-12-28 10:55:08 -0300

    feat: updated address validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :user
 
-  validates :street, :number, :city, :state, :country, :zip_code, presence: true
+  validates :street, :number, :neighborhood, :city, :state, :country, :zip_code, presence: true
 end
